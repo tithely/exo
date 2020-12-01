b2a65f1c926b40c71e77f3daa9a4f4221eed6b5c
Glenn-Ewing <glenn@glennewing.com>
2020-12-01 15:41:00 -0500

    Add Tests for Context Enabled Migrations
    1. Adds a context enabled migration (with context default, where normally an error might be thrown).
    2. Tests that the finder accepts and extracts context for use in migrations.
    3. Tests sad path, ensureing that context is undefined as expected and can be coded for as desired within the migration file(s).

--- tests/Util/FinderTest.php
+++ tests/Util/FinderTest.php
@@ -2,6 +2,8 @@
 
 namespace Exo\Util;
 
+use Exo\Operation\ViewOperation;
+
 class FinderTest extends \PHPUnit\Framework\TestCase
 {
     public function testFromPath()
@@ -15,12 +17,51 @@ class FinderTest extends \PHPUnit\Framework\TestCase
             '20190905_alter_users',
             '20190912_create_posts',
             '20200602_create_user_counts_view',
-            '20200604_create_user_level_function'
+            '20200604_create_user_level_function',
+            '20200605_create_user_counts_view_with_context'
         ], $history->getVersions());
         $this->assertEquals('users', $operations[0]->getName());
         $this->assertEquals('users', $operations[1]->getName());
         $this->assertEquals('posts', $operations[2]->getName());
         $this->assertEquals('user_counts', $operations[3]->getName());
         $this->assertEquals('user_level', $operations[4]->getName());
+    }
+
+    public function testFromPathWithContextHappyPath()
+    {
+        $finder = new Finder([
+            'tenant_database_name' => 'contextual_database_name'
+        ]);
+        $history = $finder->fromPath(__DIR__ . '/../Migrations');
+        $operations = $history->play('20200605_create_user_counts_view_with_context', '20200605_create_user_counts_view_with_context');
+
+        /* @var ViewOperation $operation */
+        $operation = $operations[0];
+
+        $this->assertEquals('user_counts', $operation->getName());
+        $this->assertEquals('alter', $operation->getOperation());
+
+        $this->assertEquals(
+            'select count(distinct id) as user_count from `contextual_database_name`.`users`',
+            $operation->getBody()
+        );
+    }
+
+    public function testFromPathWithContextSadPath()
+    {
+        $finder = new Finder([]);
+        $history = $finder->fromPath(__DIR__ . '/../Migrations');
+        $operations = $history->play('20200605_create_user_counts_view_with_context', '20200605_create_user_counts_view_with_context');
+
+        /* @var ViewOperation $operation */
+        $operation = $operations[0];
+
+        $this->assertEquals('user_counts', $operation->getName());
+        $this->assertEquals('alter', $operation->getOperation());
+
+        $this->assertEquals(
+            'select count(distinct id) as user_count from `undefined`.`users`',
+            $operation->getBody()
+        );
     }
 }
