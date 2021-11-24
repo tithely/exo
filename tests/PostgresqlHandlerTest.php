81416094614305ab15107eb6342163dbe81c01d9
Beau Durrant <beau@durrant.info>
2021-11-24 10:30:47 -0700

    Updated Handlers

--- tests/PostgresqlHandlerTest.php
+++ tests/PostgresqlHandlerTest.php
@@ -5,34 +5,34 @@ namespace Exo;
 use Exo\Tests\Traits\UsesYamlConfig;
 use PDO;
 
-class MysqlHandlerTest extends HandlerTest
+class PostgresqlHandlerTest extends HandlerTest
 {
     use UsesYamlConfig;
 
     protected function setUp(): void
     {
-        $mysql = self::yaml('handlers.mysql');
+        $pgsql = self::yaml('handlers.pgsql');
         try {
             $this->pdo = new PDO(
-                sprintf('mysql:dbname=%s;host=%s;port=%s', $mysql['name'], $mysql['host'], $mysql['port']),
-                $mysql['user'],
-                $mysql['pass']
+                sprintf('pgsql:dbname=%s;host=%s;port=%s', $pgsql['name'], $pgsql['host'], $pgsql['port']),
+                $pgsql['user'],
+                $pgsql['pass']
             );
             $this->drop();
         } catch (\Exception $e) {
-            $this->markTestSkipped('No Connection to mySQL');
+            $this->markTestSkipped('No Connection to postgreSQL');
         }
     }
 
     private function drop()
     {
-        $this->pdo->exec('DROP TABLE IF EXISTS users;');
-        $this->pdo->exec('DROP TABLE IF EXISTS posts;');
-        $this->pdo->exec('DROP VIEW IF EXISTS user_counts;');
-        $this->pdo->exec('DROP FUNCTION IF EXISTS user_level;');
-        $this->pdo->exec('DROP FUNCTION IF EXISTS user_count_function;');
-        $this->pdo->exec('DROP PROCEDURE IF EXISTS post_count;');
-        $this->pdo->exec('DROP TABLE IF EXISTS all_columns_and_types;');
+        $this->pdo->exec('DROP TABLE IF EXISTS users CASCADE;');
+        $this->pdo->exec('DROP TABLE IF EXISTS posts CASCADE;');
+        $this->pdo->exec('DROP VIEW IF EXISTS user_counts CASCADE;');
+        $this->pdo->exec('DROP FUNCTION IF EXISTS user_level CASCADE;');
+        $this->pdo->exec('DROP FUNCTION IF EXISTS user_count_function CASCADE;');
+        $this->pdo->exec('DROP PROCEDURE IF EXISTS post_count CASCADE;');
+        $this->pdo->exec('DROP TABLE IF EXISTS all_columns_and_types CASCADE;');
     }
 
     public function testFullMigration()
