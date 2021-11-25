28d8b8c04524bde3fc56ce377846ba9ac00ec776
Beau Durrant <beau@durrant.info>
2021-11-25 08:56:32 -0700

    Update index tests

--- tests/Migrations/20190901_create_users.php
+++ tests/Migrations/20190901_create_users.php
@@ -4,4 +4,4 @@ return Exo\TableMigration::create('users')
     ->addColumn('id', ['type' => 'uuid'])
     ->addColumn('username', ['type' => 'string', 'length' => 64])
     ->addColumn('password', ['type' => 'string', 'length' => 255])
-    ->addIndex('users_id_idx', ['id']);
+    ->addIndex('users', ['id'], ['unique' => true]);
--- tests/Migrations/20190905_alter_users.php
+++ tests/Migrations/20190905_alter_users.php
@@ -1,8 +1,7 @@
 <?php
 
 return Exo\TableMigration::alter('users')
-    ->addColumn('email', ['type' => 'string', 'length' => 255, 'after' => 'id'])
+    ->addColumn('email', ['type' => 'string', 'unique' => true, 'length' => 255, 'after' => 'id'])
     ->modifyColumn('password', ['type' => 'string', 'length' => 100])
     ->dropColumn('username')
-    ->addIndex('users_email_idx', ['password'])
-    ->dropIndex('users_id_idx');
+    ->dropIndex('users');
