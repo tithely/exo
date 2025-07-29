0b5666514eed3309da8171c6e9faa7c489790f4b
Ben Sinclair <ben.sinclair@yourgiving.co>
2025-07-29 11:05:02 +1000

    Fix compatibility issues with newer PHP versions

--- src/Operation/FunctionOperation.php
+++ src/Operation/FunctionOperation.php
@@ -66,13 +66,13 @@ final class FunctionOperation extends AbstractOperation implements ReversibleOpe
     public function __construct(
         string $name,
         string $operation,
-        ReturnTypeOperation $returnType = null,
+        ?ReturnTypeOperation $returnType = null,
         bool $deterministic = false,
         string $dataUse = 'READS SQL DATA',
         string $language = 'plpgsql',
         array $parameterOperations = [],
         array $variableOperations = [],
-        string $body = null
+        ?string $body = null
     ) {
         $this->name = $name;
         $this->operation = $operation;
